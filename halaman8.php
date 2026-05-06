da02e95e400738f14138f25994103adbeaaa9c82
ririnnadita <ririnnadita248228@student.smkn1kandeman.sch.id>
2026-05-06 07:37:21 +0700

    ertgrgds

--- halaman8.php
+++ halaman8.php
@@ -236,7 +236,7 @@ body{
           <img src="visi.png" class="title-img">
 
           <div class="box">
-            <p><span>✔</span><span>Terwujudnya IPNU yang aktif, berakhlak, dan berprestasi dalam organisasi maupun akademik.</span></p>
+            <p><span>✔</span><span>Menjadikan IPNU sebagai organisasi pelajar yang inspiratif, berkarakter, dan peduli terhadap lingkungan sekitar.</span></p>
           </div>
         </div>
 
@@ -244,9 +244,9 @@ body{
           <img src="misi.png" class="title-img">
 
           <div class="box">
-            <p><span>✔</span><span>Meningkatkan keaktifan anggota dalam setiap kegiatan IPNU</span></p>
-            <p><span>✔</span><span>Menumbuhkan sikap disiplin dan tanggung jawab dalam berorganisasi</span></p>
-            <p><span>✔</span><span>Mengadakan kegiatan yang mendukung prestasi pelajar</span></p>
+            <p><span>✔</span><span>Menciptakan program kerja yang inovatif dan bermanfaat.</span></p>
+            <p><span>✔</span><span>Membentuk karakter pelajar yang berakhlakul karimah.</span></p>
+            <p><span>✔</span><span>Meningkatkan kepedulian sosial melalui kegiatan kemasyarakatan.</span></p>
           </div>
         </div>
 
